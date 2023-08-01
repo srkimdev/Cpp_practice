e89ffb0dbad7b54ed55fe4464b2e938d9b89b05f
srkim <monbgqvjk@naver.com>
2023-08-01 15:43:03 +0900

    first commit

---
+++ Color.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Color {
+    int red, green, blue;
+    std::string color;
+
+    public:
+        Color() : Color(0, 0, 0, "black") {}
+        Color(int r, int g, int b, std::string c) : red(r), green(g), blue(b), color(c) {}
+        ~Color() {std::cout << color << "객체 소멸" << std::endl;}
+        void setColor(int r, int g, int b, std::string c);
+        void show();
+};
+
+void Color::show() {
+    std::cout << red << " " << green << " " << blue << std::endl;
+}
+
+int main() {
+    Color *p = new Color;
+    Color *q = new Color(0, 255, 0, "green");
+    p -> show();
+    q -> show();
+
+    delete q;
+    delete p;
+
+    return 0;
+}
