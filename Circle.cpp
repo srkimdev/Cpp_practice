90314aeb1203b3fe93d6dbdc4e77742b0308d789
srkim <monbgqvjk@naver.com>
2023-08-02 10:02:51 +0900

    using dynamic_memory_allocation

--- Circle.cpp
+++ Circle.cpp
@@ -2,32 +2,62 @@
 
 class Circle {
     int radius;
+    int size;
 
     public:
-        Circle() : radius(5) {}
-        Circle(int r) : radius(r) {}
-        void setRadius (int r) {
-            radius = r;
-        }
+        Circle() : Circle(1, 1) {}
+        Circle(int r, int s) : radius(r), size(s) {}
+        ~Circle();
+        void setRadius (int r);
         double getArea();
+        int show_radius();
 };
 
 double Circle::getArea() {
     return radius * radius * 3.14;
 }
 
-int main() {
-    Circle circleArray[3];
+void Circle::setRadius (int r) {
+    radius = r;
+}
 
-    circleArray[0].setRadius(10);
-    circleArray[1].setRadius(20);
-    circleArray[2].setRadius(30);
+int Circle::show_radius () {
+    return radius;
+}
 
-    Circle *p;
-    p = circleArray;
-
-    for (int i=0; i<3; i++) {
-        std::cout << "Circle" << i << "의 면적은 " << p -> getArea() << std::endl;
+Circle::~Circle() {
+    for (int i=0; i < size; i++) {
+        std::cout << "반지름 " << radius << " 객체 소멸" << std::endl;
     }
+}
+
+int main() {
+    int size;
+    int radius;
+    int sum = 0;
+
+    std::cout << "생성하고자 하는 원의 개수 >> ";
+    std::cin >> size;
+    Circle *p = new Circle[size];
+
+    for (int i = 1; i < size + 1; i++) {
+        std::cout << "원" << i << ": ";
+        std::cin >> radius;
+        p[i-1].setRadius(radius);
+    }
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "반지름 = " << p[i].show_radius() << "     " << "넓이 = " << p[i].getArea() << std::endl;
+        if ((p[i].getArea() > 100) && (p[i].getArea() < 200)) {
+            sum ++;
+        }
+    }
+
+    std::cout << std::endl;
+    std::cout << "면적이 100에서 200 사이인 원의 개수는 " << sum << std::endl;
+    std::cout << std::endl;
+
+    delete [] p;
+
     return 0;
 }
