a152417aa73685995cd950fd37cb5021f278134f
srkim <monbgqvjk@naver.com>
2023-08-03 10:17:27 +0900

    first commit

---
+++ Virtual_function.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class Base {
+    std::string s;
+
+    public:
+        Base() : s("기반") {
+            std::cout << "기반 클래스" << std::endl;
+        }
+
+        void what() {
+            std::cout << s << std::endl;
+        }
+};
+
+class Derived : public Base {
+    std::string s;
+
+    public:
+        Derived() : s("파생"), Base() {
+            std::cout << "파생 클래스" << std::endl;
+        }
+        
+        void what() {
+            std::cout << s << std::endl;
+        }
+};
+
+int main() {
+    std::cout << " === 기반 클래스 생성 === " << std::endl;
+    Base p;
+
+    p.what();
+
+    std::cout << " === 파생 클래스 생성 === " << std::endl;
+    Derived c;
+
+    c.what();
+
+    return 0;
+}
+
+//기반 클래스
+//기반 클래스
+//파생 클래스
+//=== 실제 객체는 Base ===
+//
+//=== 실제 객체는 Derived ===
+//
