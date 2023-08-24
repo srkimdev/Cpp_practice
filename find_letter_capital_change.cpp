0df161fdf2a01638fc0a3fe2ded42bf3ae9468e5
srkim <monbgqvjk@naver.com>
2023-08-24 17:02:27 +0900

    문자찾고 대문자로 바꾸는 프로그램

---
+++ find_letter_capital_change.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+std::string Change_capital (std::string a, char b) {
+
+    for(int j = 0; j < a.length(); j++) {
+        if(a[j] == b) {
+            a[j] = a[j] - 32; // 대문자 변환
+        }
+    }
+    return a;
+}
+
+int main() {
+
+    std::string a;
+    char b;
+
+    std::cout << "변경전 문자열 = ";
+    getline(std::cin, a);
+
+    std::cout << "문자 찾기: ";
+    std::cin >> b; 
+
+    a = Change_capital(a, b);
+
+    std::cout << "변경후 문자열 = " << a << std::endl;
+
+    return 0;
+}
